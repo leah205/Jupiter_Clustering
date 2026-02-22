fa2ab19d7f05607d300b5811597f1f20c3ff4dd4
leah205 <leahtiktin@gmail.com>
2026-02-22 10:35:24 -0700

    feat: add latitude and longitude parameter ranges for analysis

--- scripts/clustering/derived_clusters.py
+++ scripts/clustering/derived_clusters.py
@@ -1,6 +1,36 @@
-from preprocessing import get_parameter_2d_array
+
 import numpy as np
 from sklearn.mixture import GaussianMixture as GMM
+from scripts.preprocessing.preprocessing import get_pix_arr
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+#full, tied, diagonal, spherical
+
+def gmm_bic_score(estimator, X):
+    return estimator.bic(X)
+
+param_grid = {
+    "n_components": range(1, 6),
+    "covariance_type": ["tied"]
+}
+
+grid_search = GridSearchCV(GMM(), param_grid = param_grid, scoring = gmm_bic_score)
+
+X = get_pix_arr(["NH3", "PCld"])
+grid_search.fit(X)
 
 
-def perform_clustering():
+df = pd.DataFrame(grid_search.cv_results_)[
+    ["param_n_components", "param_covariance_type", "mean_test_score"]
+]
+df["mean_test_score"] = -df["mean_test_score"]
+df = df.rename(
+    columns={
+        "param_n_components": "Number of components",
+        "param_covariance_type": "Type of covariance",
+        "mean_test_score": "BIC score",
+    }
+)
+print(df.sort_values(by="BIC score").head())
+
+
--- scripts/preprocessing/preprocessing.py
+++ scripts/preprocessing/preprocessing.py
@@ -3,17 +3,18 @@ from os import listdir
 from astropy.io import fits
 import numpy as np
 from astropy.wcs import WCS
-#create errors
+#add dimension error
+#add get_pix_arr test
+#regression tests?
 def get_radiance_arr(file):
     hdul = fits.open(file)
-    
-    #print(hdul[0].data)
     return hdul[0].data
 
 def get_wcs(file):
     print(file)
     hdul = fits.open(file)
     hdr = hdul[0].header
+    print(WCS(hdr))
     return WCS(hdr)
 
 def is_files_aligned(file_arr):
@@ -32,29 +33,49 @@ def get_file_path(keyword, dir):
         if keyword in f:
             return dir + "/" + f
            
-def get_parameter_nd_array(keyword_arr):
-    
+def get_parameter_2d_array(keyword_arr, latLims = [45, 135], lonRng = [0, 360]):
+    '''
+    Purpose:
+
+
+    Parameters
+    ------------
+    '''
+   
     dir_path = config["input"]
     file_arr = []
     file_name_arr = []
     for keyword in keyword_arr:
         file = get_file_path(keyword, dir_path)
-        file_arr.append(get_radiance_arr(file))
         file_name_arr.append(file)
 
-
-    #nh3_file = get_file_path("NH3", dir_path)
-    #cld_file = get_file_path("Cld", dir_path)
-    
-
-    res = np.array(file_arr)
     if(is_files_aligned(np.array(file_name_arr)) == False):
         raise TypeError("files are not mapped to the same coordinates")
-    print(res.ndim)
-    print(res.shape)
+
+    for file_name in file_name_arr:
+        radiance_arr = get_radiance_arr(file_name)
+        file_arr.append(subset_map(radiance_arr, latLims, lonRng).flatten())
+    res = np.array(file_arr)
     return res
 
+def get_pix_arr(arr):
+    parameter_arr = get_parameter_2d_array(arr)
+    return np.column_stack((parameter_arr))
+
+def subset_map(map, latLims, lonRng):
+    '''
+    Purpose:
+
+
+    Parameters
+    ------------
+    '''
+    scale = int(map.shape[0]/ 180)
+    latLims = np.array(latLims)* scale
+    lonRng=lonRng*scale
+    print(latLims, lonRng)
+    return map[latLims[0]:latLims[1], lonRng[0]: lonRng[1]]
 
 
 
-print(type(get_parameter_nd_array(["NH3", "PCld"])))
+print(type(get_parameter_2d_array(["NH3", "PCld"])))
