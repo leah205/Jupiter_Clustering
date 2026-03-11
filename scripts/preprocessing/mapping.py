8ef9ad0327466123c68bf43140be7e969f7adcdf
leah205 <leahtiktin@gmail.com>
2026-03-11 10:50:00 -0600

    feat: create labeled spatial map visualization

--- scripts/preprocessing/mapping.py
+++ scripts/preprocessing/mapping.py
@@ -6,46 +6,30 @@ import scripts.clustering.clusters as CL
 from sklearn.metrics import silhouette_score
 from config.config import config
 
-def createLonMap(LonLims, shape):
-    #oned_map = np.linspace(LonLims[0], LonLims[1], shape[1])
-    #res = np.tile(oned_map, (shape[0], 1))
-    print("hi")
-
-
-def createLatMap(LatLims, shape):
-    #oned_map =  np.lonspace(LatLims[0], LatLims[1], shape[0])
-    #res = np.tile(oned_map, (1, shape[1]))
-    print("yo")
-
-def create_axis(LonLims, LatLims, Ltitle, LonSys):
-    #fig3,axs3=pl.subplots(1,2,figsize=(figxy[0],figxy[1]), dpi=150, facecolor="white")
-    #fig3.suptitle(suptitle,x=0.5,ha='center',color='k')
-    #fig3.suptitle(mapyhdr["DATE-OBS"].replace("_"," ")+", CM"+LonSys+"="
-    #              +str(int(PlotCM)),x=0.5,ha='center',color='k')
-    fig3, axs3 = pl.subplots()
-
-    axs3[0].grid(linewidth=0.2)
-    axs3[0].ylim=[-45.,45.]
-    axs3[0].xlim=[360-LonLims[0],360-LonLims[1]]
-    axs3[0].set_xticks(np.linspace(450,0,31), minor=False)
+def create_axis(LatLims, LonLims, Ltitle, LonSys):
+    fig3,axs3=pl.subplots(dpi=150, facecolor="white")
+    axs3.grid(linewidth=0.2)
+    axs3.ylim=[LatLims[0] ,LatLims[1]]
+    axs3.xlim=[360-LonLims[0],360-LonLims[1]]
+    axs3.set_xticks(np.linspace(450,0,31), minor=False)
     xticklabels=np.array(np.mod(np.linspace(450,0,31),360))
-    axs3[0].set_xticklabels(xticklabels.astype(int))
-    axs3[0].set_yticks(np.linspace(-45,45,7), minor=False)
-    axs3[0].tick_params(axis='both', which='major', labelsize=9)
-    axs3[0].set_ylabel("Planetographic Latitude (deg)",fontsize=10)
-    axs3[0].set_xlabel("Sys. "+LonSys+" Longitude (deg)",fontsize=10)
-    axs3[0].set_title(Ltitle,fontsize=10)
-    axs3[0].set_adjustable('box') 
-    axs3[1].set_adjustable('box')
-    return(axs3)
+    axs3.set_xticklabels(xticklabels.astype(int))
+    axs3.set_yticks(np.linspace(-45,45,7), minor=False)
+    axs3.tick_params(axis='both', which='major', labelsize=9)
+    axs3.set_ylabel("Planetographic Latitude (deg)",fontsize=10)
+    axs3.set_xlabel("Sys. "+ str(LonSys) +" Longitude (deg)",fontsize=10)
+    axs3.set_title(Ltitle,fontsize=10)
+    axs3.set_adjustable('box') 
+    return axs3
 
-def plot_patch(patch, LatLims, LonLims, axis, colorscale, vn = 0.1, vx = 0.2, n = 6):
+def plot_patch(patch, LatLims, LonLims, axis, colorscale, vn = 0, vx = 10, n = 10):
     '''
     Purpose:
         to plot a patch with appropriate longitude/latitude scales
     '''
     np.nan_to_num(patch, copy=False, nan=0.0, posinf=0.0, neginf=0.0)
     tx=np.linspace(vn,vx,n,endpoint=True)
+    print(patch)
     show=axis.imshow(patch, colorscale, origin='upper',vmin=vn,vmax=vx,  
                extent=[360-LonLims[0],360-LonLims[1],90-LatLims[1],
                        90-LatLims[0]],
@@ -60,11 +44,13 @@ def create_cluster_map(keywords, param_ranges, n_comp = 5, cov_type = "full",
     pred = CL.create_clusters(input_arr[:, [0,1]], cov_type, n_comp)
     print("silhouette score: " + str(silhouette_score(input_arr, pred)))
     cluster_map = create_cluster_arr(input_arr, subset_shape, pred)
-    hdul = fits.HDUList()
-    hdul.append(fits.PrimaryHDU())
-    hdul[0].data = cluster_map
-    #mp.createLonMap(lngRng, hdul[0].shape[1])
-    hdul.writeto(f'{config["output"]}/cluster_maps/spatial_map_{cov_type}_{n_comp}.fits', overwrite= True)
+    LTitle = f'spatial_cluster_map_{cov_type}_{n_comp}'
+    axis = create_axis([latRng[0] - 90, 90 - latRng[1]], lngRng, LTitle, cm_num)
+    plot_patch(cluster_map, latRng, lngRng, axis, "jet")
+    fig = axis.get_figure()
+    fig.savefig(f'{config["output"]}/cluster_maps/spatial_map_{cov_type}_{n_comp}.png')
+
+
 
 def create_cluster_arr(input_arr, subset_shape, pred):
     subset_length = subset_shape[0] * subset_shape[1]
@@ -76,5 +62,6 @@ def create_cluster_arr(input_arr, subset_shape, pred):
         index, cluster = int(indexed_clusters[r][0]), int(indexed_clusters[r][1])
         oned_mapped_clusters[index] = cluster
     mapped_clusters = oned_mapped_clusters.reshape(subset_shape)
+    return mapped_clusters
 
 create_cluster_map(["NH3", "PCld"], [[30, 250], [1000, 2500]])
