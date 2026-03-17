2ca9c01529a397ab1e63a34ab585da70c8ac2ca5
leah205 <leahtiktin@gmail.com>
2026-03-17 09:06:01 -0600

    add scaler to clusterimg pipeline to standardize distamces

--- scripts/processes.py
+++ scripts/processes.py
@@ -11,7 +11,7 @@ from matplotlib.colors import ListedColormap
 
 
 def output_cluster_map_and_plot(date, keywords, param_ranges, n_comp = 5, cov_type = "full", 
-                           latRng = [65, 115], lngRng = [0, 50], cm_num = 3):
+                           latRng = [65, 115], lngRng = [0, 360], cm_num = 3):
     [input_arr, subset_shape] = pre.get_input_array(keywords, param_ranges, latRng, lngRng, cm_num)
     pred = CL.create_clusters(input_arr[:, [0,1]], cov_type, n_comp)
     sil_score = str(round(silhouette_score(input_arr, pred), 3))
@@ -41,7 +41,10 @@ def output_cluster_plot(date, keywords, param_ranges, n_comp = 5, cov_type = "fu
     PL.create_cluster_plot(date, keywords, input_arr, pred, sil_score, n_comp, cov_type, latRng, lngRng, cmap, cm_num)
 
 
-output_cluster_map_and_plot("20251214", ["NH3", "PCld"], [[0, 300], [1000, 3000]], 5)
+output_cluster_map_and_plot("20251214", ["NH3", "PCld"], [[0, 300], [1000, 3000]], 2)
+#output_cluster_map_and_plot("20251214", ["NH3", "PCld"], [[0, 300], [1000, 3000]], 4)#
+
+
 #output_cluster_map("20251214", ["NH3", "PCld"], [[0, 300], [1000, 3000]], 5)
 #output_cluster_plot("20251214", ["NH3", "PCld"], [[0, 300], [1000, 3000]], 5)
     
