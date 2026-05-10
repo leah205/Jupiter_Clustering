692035ab495a69cef61ebb7f52a09f2f2643ecde
leah205 <leahtiktin@gmail.com>
2026-05-10 09:04:36 -0600

    change label assignments to means matching and test

--- scripts/cluster_stats.py
+++ scripts/cluster_stats.py
@@ -2,6 +2,7 @@
 import numpy as np
 import scripts.preprocessing.preprocessing as pre
 
+
 def get_stat(cluster_arr, dim_arr, indices, n_clusters, dim_name):
     '''
         Purpose: Takes a cluster and outputs the mean and standard deviation of that cluster in a certain dimension
@@ -36,21 +37,6 @@ def get_stat(cluster_arr, dim_arr, indices, n_clusters, dim_name):
     return res
 
 def get_all_stats(pred, keywords, indices, n_comp, latRng, lngRng, cm_num):
-    cluster_arr = np.array(pred)
-    indices = indices.astype(np.int64)
-    if(not (cluster_arr.shape == indices.shape) and cluster_arr.ndim == 1):
-        raise TypeError("Index array length must match cluster array length")
-    
-    
-    for key in keywords:
-        map =  pre.get_patch(key, latRng, lngRng, cm_num)
-        dim_arr = map.flatten()
-        get_stat(cluster_arr, dim_arr, indices, n_comp, key)
-
-    
-
-def get_cluster_stat(cluster_arr, dim_arr, indices, n_clusters, dim_name):
-    # flatten arrays here
     '''
     Purpose: Preprocesses data and passes it into a function to generate list of means and standard deviations for the clusters
 
@@ -63,17 +49,55 @@ def get_cluster_stat(cluster_arr, dim_arr, indices, n_clusters, dim_name):
 
     Returns
     --------------
-    List of mean and standard deviation pairs of each cluster in the dimension
+    List of Lists of mean and standard deviation pairs for each cluster for each dimension
 
 
     
     '''
-    cluster_arr = np.array(cluster_arr)
+    cluster_arr = np.array(pred)
     indices = indices.astype(np.int64)
     if(not (cluster_arr.shape == indices.shape) and cluster_arr.ndim == 1):
         raise TypeError("Index array length must match cluster array length")
-    dim_arr = dim_arr.flatten()
-    return get_stat(cluster_arr, dim_arr, indices, n_clusters, dim_name)
+    
+    res = []
+    for key in keywords:
+        map =  pre.get_patch(key, latRng, lngRng, cm_num)
+        dim_arr = map.flatten()
+        res.append(get_stat(cluster_arr, dim_arr, indices, n_comp, key))
+    return np.array(res)
+    
+def reassign_clusters(pred, stats, param_ranges):
+    """
+    Parameters
+    -----------
+    pred
+        np array of cluster assignments
+    stats
+        np array of means with axis 0 as clusters and axis 1 as dimension
+    
+    
+    """
+    print(stats)
+    cluster_means = quantify_clusters(stats, param_ranges)
+    indices = np.argsort(cluster_means)
+    return indices[pred]
+    
+def quantify_clusters(stats, param_ranges):
+    # convert param_ranges to numpy array
+    """
+    Purpose:  Find the mean of all means of clusters in all dimensions 
+
+    Parameters:
+    stats - nd array, first axis is dimension, second axis, is cluster means for that dimension
+    param_ranges - similiarly ordered cross-dimensional array with min and max value for each dimension
+    """
+    print(stats)
+    param_swapped = np.swapaxes(param_ranges, 0, 1)
+    normed_stats = (stats - param_swapped[0]) / (param_swapped[1] - param_swapped[0])
+    return np.mean(normed_stats, axis = 1)
+
+    
+
 
 
 
@@ -82,4 +106,4 @@ def get_cluster_stat(cluster_arr, dim_arr, indices, n_clusters, dim_name):
 
 #def get_indice_cluster_stats(cluster_arr, ):
     
-#def get_ammonia_cloud_cluster_stats():
+#def get_ammonia_cloud_cluster_stats()
